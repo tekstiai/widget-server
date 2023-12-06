6e215f1f52081d42cb4f7dd8803cf008dfe0f96a
Markus T <markus.talka@gmail.com>
2023-12-06 18:37:51 +0200

    Formatting, development specific code

---
+++ Readme.md
@@ -0,0 +1,55 @@
+# TekstiAI Widget authentication server
+
+## Introduction
+
+This README provides guidance on how to implement and use the TekstiAI Widget Authentication Server. This server, built with Node.js and Express, is designed to handle authentication and relay questions to the TekstiAI widget.
+
+## Prerequisites
+
+- Node.js installed
+- Knowledge of Express.js and Node.js environments
+- Access to the server environment variables (CLIENT_ID, CLIENT_SECRET, TOKEN_ENDPOINT, SSL_KEY_FILE, SSL_CRT_FILE)
+
+## Installation
+
+1. **Clone the Repository**
+    
+    - Clone or download the server code to your local environment.
+2. **Install Dependencies**
+    
+    - Navigate to the project directory and run `npm install` to install all required dependencies.
+3. **Environment Setup**
+    
+    - Create a `.env` file in the root of the project.
+    - Add the necessary environment variables: `CLIENT_ID`, `CLIENT_SECRET`, `TOKEN_ENDPOINT`, and `PORT`
+    - You can omit development time code, which also eliminates need for `SSL_KEY_FILE`, `SSL_CRT_FILE`
+
+## Running the Server
+
+- **Development Mode**: Run `npm run dev` to start the server in development mode with nodemon.
+- **Production Mode**: Run `npm start` to start the server in production mode.
+
+## Usage
+
+1. **Authentication Endpoint (`/getAccessToken`)**:
+    
+    - This endpoint fetches an access token.
+    - Use this token in subsequent requests to authenticate with the TekstiAI widget.
+2. **Relay Question Endpoint (`/relayQuestion`)**:
+    
+    - Allows sending questions to the TekstiAI widget.
+    - Requires the following in the request body:
+        - `apiUrl`: The URL to which the question should be sent.
+        - `organisationKey`: Your organization's unique key.
+        - `questionData`: The data of the question being sent.
+
+## Security Considerations
+
+- Ensure SSL/TLS is configured correctly for secure communication in production.
+- Keep all environment variables and sensitive information secure.
+
+## Troubleshooting
+
+- Check server logs for error messages.
+- Ensure all environment variables are correctly set.
+- Verify network connectivity and endpoint accessibility.
--- server.js
+++ server.js
@@ -1,58 +1,106 @@
-const express = require('express');
-const axios = require('axios');
-const helmet = require('helmet');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000; // Use environment variable for port
+const port = process.env.PORT || 3000;
 
-app.use(helmet()); // Security middleware to set various HTTP headers
-app.use(cors()); // Enable CORS for all routes
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 let cachedToken = {
-    value: null,
-    expiry: null
+  value: null,
+  expiry: null,
 };
 
 async function fetchAccessToken() {
-    try {
-        const params = new URLSearchParams();
-        params.append('grant_type', 'client_credentials');
-        params.append('client_id', process.env.CLIENT_ID);
-        params.append('client_secret', process.env.CLIENT_SECRET);
-        params.append('scope', 'https://graph.microsoft.com/.default');
+  try {
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+    params.append("client_id", process.env.CLIENT_ID);
+    params.append("client_secret", process.env.CLIENT_SECRET);
+    params.append("scope", "https://graph.microsoft.com/.default");
 
-        const response = await axios.post(process.env.TOKEN_ENDPOINT, params, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        });
+    const response = await axios.post(process.env.TOKEN_ENDPOINT, params, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
 
-        const expiresIn = response.data.expires_in;
-        cachedToken = {
-            value: response.data.access_token,
-            expiry: new Date(new Date().getTime() + expiresIn * 1000) // Setting the expiry time
-        };
-    } catch (error) {
-        console.error('Error fetching token:', error);
-        throw new Error('Error fetching token');
-    }
+    const expiresIn = response.data.expires_in;
+    cachedToken = {
+      value: response.data.access_token,
+      expiry: new Date(new Date().getTime() + expiresIn * 1000),
+    };
+  } catch (error) {
+    console.error("Error fetching token:", error);
+    throw new Error("Error fetching token");
+  }
 }
 
-app.get('/getAccessToken', async (req, res) => {
-    try {
-        console.log('Cached token:', cachedToken)
-        if (!cachedToken.value || new Date() >= cachedToken.expiry) {
-            await fetchAccessToken();
-        }
-        res.json({ accessToken: cachedToken.value });
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
+app.get("/getAccessToken", async (req, res) => {
+  try {
+    if (!cachedToken.value || new Date() >= cachedToken.expiry) {
+      await fetchAccessToken();
     }
+    res.json({ accessToken: cachedToken.value });
+  } catch (error) {
+    console.error("Error in /getAccessToken:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.post("/relayQuestion", async (req, res) => {
+  const { apiUrl, organisationKey, questionData } = req.body;
+  try {
+    if (!cachedToken.value || new Date() >= cachedToken.expiry) {
+      await fetchAccessToken();
+    }
+
+    if (!cachedToken.value) {
+      res.status(500).send("Failed to fetch access token");
+      return;
+    }
+
+    const response = await axios.post(
+      `${apiUrl}/question?apiKey=` + organisationKey,
+      questionData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Access-Token": `${cachedToken.value}`,
+          Accept: "application/json",
+        },
+      }
+    );
+
+    res.send(response.data);
+  } catch (error) {
+    console.error("Failed to relay question", error);
+    res.status(500).send("Failed to relay question");
+  }
 });
+
+if (process.env.NODE_ENV === "development") {
+  // Development-specific HTTPS setup
+  const https = require("https");
+  const fs = require("fs");
+
+  const credentials = {
+    key: fs.readFileSync(process.env.SSL_KEY_FILE),
+    cert: fs.readFileSync(process.env.SSL_CRT_FILE),
+    rejectUnauthorized: false,
+  };
+
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(port, () => {
+    console.log(`HTTPS Server running on port ${port}`);
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`HTTP Server running on port ${port}`);
+  });
+}
