4b157d4d0e8fe1312ec165cdccb0c9fe55ddb79c
Markus T <markus.talka@gmail.com>
2023-12-06 15:20:54 +0200

    Fetch token from cache if available

--- server.js
+++ server.js
@@ -11,27 +11,44 @@ app.use(helmet()); // Security middleware to set various HTTP headers
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
 
-const clientID = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const tokenEndpoint = process.env.TOKEN_ENDPOINT;
+let cachedToken = {
+    value: null,
+    expiry: null
+};
 
-app.get('/getAccessToken', async (req, res) => {
+async function fetchAccessToken() {
     try {
         const params = new URLSearchParams();
         params.append('grant_type', 'client_credentials');
-        params.append('client_id', clientID);
-        params.append('client_secret', clientSecret);
+        params.append('client_id', process.env.CLIENT_ID);
+        params.append('client_secret', process.env.CLIENT_SECRET);
         params.append('scope', 'https://graph.microsoft.com/.default');
 
-        const response = await axios.post(tokenEndpoint, params, {
+        const response = await axios.post(process.env.TOKEN_ENDPOINT, params, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         });
 
-        res.json({ accessToken: response.data.access_token });
+        const expiresIn = response.data.expires_in;
+        cachedToken = {
+            value: response.data.access_token,
+            expiry: new Date(new Date().getTime() + expiresIn * 1000) // Setting the expiry time
+        };
     } catch (error) {
         console.error('Error fetching token:', error);
+        throw new Error('Error fetching token');
+    }
+}
+
+app.get('/getAccessToken', async (req, res) => {
+    try {
+        console.log('Cached token:', cachedToken)
+        if (!cachedToken.value || new Date() >= cachedToken.expiry) {
+            await fetchAccessToken();
+        }
+        res.json({ accessToken: cachedToken.value });
+    } catch (error) {
         res.status(500).send('Internal Server Error');
     }
 });
